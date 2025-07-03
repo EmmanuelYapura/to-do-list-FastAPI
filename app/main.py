5d1fea2fe6452d14737bc9c7bb98489fdc3e04dd
Emmanuel Yapura <eyapura96@gmail.com>
2025-07-03 16:31:59 -0300

    agregamos archivos staticos para ser consumidos por fastapi

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from app.controllers.tasks_controllers import router
 from app.repositories.database import Base, engine
 
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
 Base.metadata.create_all(bind=engine)
 app.include_router(router=router)
