c2b756343f7b9315087783e12fadb2d70ecea625
Emmanuel Yapura <eyapura96@gmail.com>
2025-11-13 15:31:19 -0300

    agregando permisos CORS, titulo a la app y nuevas rutas para consumir con react

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.tasks_controllers import router
+from app.controllers.tasks_api import router_api
 from app.repositories.database import Base, engine
 
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+app = FastAPI(title = "To Do List API", version="1.0.0")
+
+# agregando CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173","http://localhost:3000"], #modificar origin proximamente
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.mount('/static', StaticFiles(directory='app/static'), 'static')
 Base.metadata.create_all(bind=engine)
 app.include_router(router=router)
+app.include_router(router=router_api)
+
+@app.get("/")
+def root():
+    return{"message": "API funcionando correctamente con CORS activo"}
