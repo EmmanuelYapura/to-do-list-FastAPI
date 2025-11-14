717b016a887abfa28c902958a958cbabdb246e85
Emmanuel Yapura <eyapura96@gmail.com>
2025-11-14 05:39:19 -0300

    refactorización de los endpoints de FastAPI para utilizar los esquemas de Pydantic y simplificar respuestas eliminando JSONResponse

--- app/controllers/tasks_api.py
+++ app/controllers/tasks_api.py
@@ -1,50 +1,37 @@
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.services.tasks_service import TaskService
 from app.repositories.database import get_db
-from app.schema.schemas import Task
+from app.schema.schemas import TaskResponse, Task, TaskUpdate
 
 router_api = APIRouter()
 service = TaskService()
 
-@router_api.get("/api/tasks")
+@router_api.get("/api/tasks", response_model=list[TaskResponse])
 def get_tasks(db: Session = Depends(get_db)):
     tasks = service.get_tasks(db)
-    return JSONResponse(content=[
-        {
-            "id": t.id,
-            "nombre": t.nombre,
-            "completa": t.completa,
-            "importante": t.importante
-        } for t in tasks
-    ])
+    return tasks
 
-@router_api.get("/api/tasks/{id}")
+@router_api.get("/api/tasks/{id}", response_model=TaskResponse)
 def get_task(id: int, db: Session = Depends(get_db)):
     task = service.get_task(db, id)
     if not task:
         raise HTTPException(status_code=404, detail="Tarea no encontrada")
-    return JSONResponse(content={
-        "id": task.id,
-        "nombre": task.nombre,
-        "completa": task.completa,
-        "importante": task.importante
-    })
+    return task
 
-@router_api.post("/api/tasks")
+@router_api.post("/api/tasks", response_model=TaskResponse)
 def create_task(task: Task, db: Session = Depends(get_db)):
     new_task = service.create_task(db, task)
-    return JSONResponse(content={"message": "Tarea creada", "task_id": new_task.id})
+    return new_task
 
-@router_api.put("/api/tasks/{id}")
-def update_task(id: int, task: Task, db: Session = Depends(get_db)):
+@router_api.put("/api/tasks/{id}", response_model=TaskResponse)
+def update_task(id: int, task: TaskUpdate, db: Session = Depends(get_db)):
     updated = service.update_task(db, id, task)
-    return JSONResponse(content={"message": "Tarea actualizada", "task_id": updated.id})
+    return updated
 
 @router_api.delete("/api/tasks/{id}")
 def delete_task(id: int, db: Session = Depends(get_db)):
     deleted = service.delete_task(db, id)
     if not deleted:
         raise HTTPException(status_code=404, detail="Tarea no encontrada")
-    return JSONResponse(content={"message": "Tarea eliminada"})
+    return  {"message": "Tarea eliminada"}
