b615928fe826be29f1b69eee321f151da2c981ca
Emmanuel Yapura <eyapura96@gmail.com>
2025-11-14 17:18:17 -0300

    agrega funciones para manejar los metodos http create, update y delete

--- to-do-list-react/src/api/tasksApi.js
+++ to-do-list-react/src/api/tasksApi.js
@@ -1,12 +1,50 @@
-const API_URL = 'http://localhost:8000/api/tasks';
+const API_URL = "http://localhost:8000/api/tasks";
 
 export async function getTasks() {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error("Error al obtener tareas");
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function createTask(task) {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(task),
+    });
+    if (!response.ok)
+      throw new Error("Error al crear tarea: ", response.status);
+    return await response.json();
+  } catch (error) {
+    console.error("CreateTask error: ", error);
+    throw error;
+  }
+}
+
+export async function updateTask(id, updateData) {
+  try {
+    await fetch(`${API_URL}/${id}`, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(updateData),
+    });
+  } catch (error) {
+    console.error("Error al actualizar tarea importante: ", error);
+  }
+}
+
+export async function deleteTask(id) {
     try{
-        const response = await fetch(API_URL);
-        if (!response.ok) throw new Error("Error al obtener tareas");
-        return await response.json();
+        await fetch(`${API_URL}/${id}`, {
+            method: "DELETE"
+        });
     }catch(error){
-        console.error(error)
-        return [];
+        console.log("Error al eliminar tarae: ", error);
     }
 }
