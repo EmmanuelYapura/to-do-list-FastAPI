7129903e0dcaa1e956d6a0635e863655d82af98d
Emmanuel Yapura <eyapura96@gmail.com>
2025-11-14 05:37:58 -0300

    agrega esquemas pydantic para manejar las estructuras de datos

--- app/schema/schemas.py
+++ app/schema/schemas.py
@@ -1,7 +1,19 @@
 # Esta capa es para los esquemas que voy a utilizar como base model
 from pydantic import BaseModel
+from typing import Optional
 
 class Task(BaseModel):
     nombre: str
     completa: bool
     importante: bool
+
+class TaskUpdate(BaseModel):
+    nombre: Optional[str] = None
+    completa: Optional[bool] = None
+    importante: Optional[bool] = None
+
+class TaskResponse(Task):
+    id: int
+
+    class Config:
+        orm_mode = True
