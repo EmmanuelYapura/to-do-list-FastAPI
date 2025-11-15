c66465a9705a44ba546e6bad320b1ca8aaa361e2
Emmanuel Yapura <eyapura96@gmail.com>
2025-11-15 19:33:11 -0300

    agrega validacion de tareas vacias y  elimina opcion de completa

--- to-do-list-react/src/components/TaskForm.jsx
+++ to-do-list-react/src/components/TaskForm.jsx
@@ -1,38 +1,46 @@
 import { useState } from "react"
 import { createTask } from "../api/tasksApi"
+import '../styles/tasks.css'
+
 
 export function TaskForm({addTask}){
+    const [errorMessage, setErrorMessage] = useState(false);
     const [nombre, setNombre] = useState("")
-    const [completa, setCompleta] = useState(false)
     const [importante, setImportante] = useState(false)
+    let completa = false
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const newTask = {nombre, completa, importante}
-        const data = await createTask(newTask)
-        addTask(data)
+        if(nombre.trim()){
+            const newTask = {nombre, completa, importante}
+            const data = await createTask(newTask)
+            addTask(data)
+    
+            setNombre("")
+            setImportante(false)
+            setErrorMessage(false)
+            return
+        }
 
-        setNombre("")
-        setCompleta(false)
-        setImportante(false)
+        setErrorMessage(true)
     }
 
     return(
         <section>
-            <h2>Ingresa una tarea: </h2>
             <form onSubmit={handleSubmit}>
-                <input onChange={(e) => setNombre(e.target.value)} value={nombre} type="text" name="nombre" placeholder="Ingrese su tarea..." />
-                <label htmlFor="checkbox-label">
-                    <input checked={completa} onChange={(e) => setCompleta(e.target.checked)} type="checkbox" name="completa"/>
-                    Completada
-                </label>
-                <label htmlFor="checkbox-label">
+                <input onChange={(e) => setNombre(e.target.value)} value={nombre} type="text" name="nombre" placeholder="Ingrese una nueva tarea..." />
+                <div className="form-options">
+                <label>
                     <input checked={importante} onChange={e => setImportante(e.target.checked)} type="checkbox" name="importante"/>
                     Importante
                 </label>
                 <button type="submit">Agregar</button>
+                </div>
             </form>
+            <div className={`div-error ${errorMessage? "text-error" : "hidden"}`}>
+                <p>No puede agregarse una cadena vacia</p>
+            </div>
         </section>
     )
 }
