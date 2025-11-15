4e94448989b05625c7c0cd22a618ffdd4b7a52ac
Emmanuel Yapura <eyapura96@gmail.com>
2025-11-15 19:30:52 -0300

    agrega iconos para controlar tareas completas e importantes

--- to-do-list-react/src/components/TaskItem.jsx
+++ to-do-list-react/src/components/TaskItem.jsx
@@ -1,23 +1,104 @@
-import '../styles/tasks.css'
+import "../styles/tasks.css";
 
-export function TaskItem({task, onDelete, onToggleAttribute}){
-    return(
-        <div className="task-item">
-            <h4>{task.nombre}</h4>
-            <p className={task.completa? "done" : "pending"}
-            onClick={() => onToggleAttribute(task.id, {completa: !task.completa})}
-            style={{cursor: "pointer"}}
-            >completada: {task.completa? "SI" : "NO"}</p>
-            <p className={task.importante? "important" : "normal"}
-            onClick={() => onToggleAttribute(task.id, {importante: !task.importante})}
-            style={{cursor: "pointer"}}
-            >importante: {task.importante? "SI" : "NO"}</p>
-            <button
-                className="btn-delete"
-                onClick={() => onDelete(task.id)}
+export function TaskItem({ task, onDelete, onToggleAttribute }) {
+  return (
+    <div className={`task-item ${task.completa? "isComplete": ""}`}>
+      <h4>{task.nombre}</h4>
+      <div className="item-options">
+        <p
+          onClick={() =>
+            onToggleAttribute(task.id, { completa: !task.completa })
+          }
+          style={{ cursor: "pointer" }}
+        >
+          {task.completa ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="#28c309"
+              className="icon icon-tabler icons-tabler-filled icon-tabler-circle-check"
             >
-                Eliminar
-            </button>
-        </div>
-    )
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-1.293 5.953a1 1 0 0 0 -1.32 -.083l-.094 .083l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.403 1.403l.083 .094l2 2l.094 .083a1 1 0 0 0 1.226 0l.094 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#28c309"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-circle"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            </svg>
+          )}
+        </p>
+        <p
+          onClick={() =>
+            onToggleAttribute(task.id, { importante: !task.importante })
+          }
+          style={{ cursor: "pointer" }}
+        >
+          {task.importante ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="#f0e80bff"
+              className="icon icon-tabler icons-tabler-filled icon-tabler-star"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#f0e80bff"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-star"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+            </svg>
+          )}
+        </p>
+        <button className="btn-delete" onClick={() => onDelete(task.id)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#000000"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-trash"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 7l16 0" />
+            <path d="M10 11l0 6" />
+            <path d="M14 11l0 6" />
+            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 }
