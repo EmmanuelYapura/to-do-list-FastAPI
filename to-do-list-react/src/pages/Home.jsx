d520aafd72d6cc7e8548d7f5843ffca614d2aefb
Emmanuel Yapura <eyapura96@gmail.com>
2025-11-15 19:29:52 -0300

    Elimina titulo

--- to-do-list-react/src/pages/Home.jsx
+++ to-do-list-react/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { TaskList } from "../components/TaskList";
 function Home (){
     return(
         <main className="home">
-            <h1>To-Do List con React y FastAPI</h1>
             <TaskList />
         </main>
     )
